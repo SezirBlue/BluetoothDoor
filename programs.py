07add3c93192e467d09fc1dcec08ddead88e40d4
SezirBlue <rinejames@gmail.com>
2018-11-11 22:17:21 -0900

    added comments

--- programs.py
+++ programs.py
@@ -1,13 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
+# Control the servo for the lock based 
+#on whether or not bluetooth device is
+# in range
+#
+
+#open file that holds the lock position
 postStatus=open("postStatus", "r")
+#open file that holds the position the lock should be
 action=open("status", "r")
 
+
+#set status and postion
 status=action.read()
 position=postStatus.read()
 
+#open status for writing: this is useful later
 postW=open("postStatus", "w")
+
+#init motors
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
@@ -15,15 +27,17 @@ GPIO.setup(12, GPIO.OUT)
 pwm = GPIO.PWM(12,50)
 
 
-
 print ("This is status:", status, "This is position:", position)
 #print(status)
-if(status == 'close\n'):
+
+
+#if the lock should be closed
+if(status == 'close\n'):#and is closed
     if (position == 'close\n'):
         time.sleep(1)
-        print("do nothing")
+        print("do nothing")#do nothing
         postW.write(position)
-    elif (position == 'open\n'):
+    elif (position == 'open\n'):#but is open
         print("close")
         pwm.start(2.5)
         pwm.ChangeDutyCycle(2.5)#close
@@ -31,16 +45,16 @@ if(status == 'close\n'):
         time.sleep(1)
         
 
-elif (status == 'open\n'):
-    if (position =='open\n'):
+elif (status == 'open\n'):#if the lock should be open
+    if (position =='open\n'):#and is open
         print("do nothing")
-        postW.write(position)
+        postW.write(position)#do nothing
         time.sleep(1)
-    elif(position == 'close\n'):
+    elif(position == 'close\n'):#but is is closed
         print("open")
         pwm.start(7.5)
         pwm.ChangeDutyCycle(7.5)#open
         postW.write("open\n")
         time.sleep(1)
-
+#end
 GPIO.cleanup()
